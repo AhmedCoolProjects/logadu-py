662bb453cc2181d688f6aa3849e61f5ce39145cf
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2025-08-23 15:42:39 +0100

    new commands

--- logadu/run/dl2.sh
+++ logadu/run/dl2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Usage: bash scripts/run_logadu_grid.sh <model> <paradigm> <PATH_DIR>
+# DeepLog: bash scripts/run_logadu_grid.sh <model> <paradigm> <PATH_DIR>
 # Example: bash scripts/run_logadu_grid.sh logbert semi /data/logs
 if [[ $# -lt 3 ]]; then
   echo "Usage: $0 <model> <paradigm> <PATH_DIR>"
@@ -15,11 +15,11 @@ MODEL="$1"
 PARADIGM="$2"
 PATH_DIR="$3"
 
-WIN_SIZES=(5 30)
+WIN_SIZES=(30)
 
 
 DATASETS=(
-  "Linux24APT" "santos" "wardbeck" "shaw" "wilson" "fox" "harrison" "russellmitchell" known_c1 known_c2 known_c3 known_c4 known_c5 known_c6 known_c7 known_c8
+"fox" "santos" "harrison" "russellmitchell" known_c1 known_c2 known_c3 known_c4 known_c5 known_c6 known_c7 known_c8
 )
 
 for win in "${WIN_SIZES[@]}"; do
