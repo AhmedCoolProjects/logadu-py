41ea1785a98d63b2d2b28fdc9dd3a585051c5881
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2025-08-23 15:53:48 +0100

    checkpoint

--- logadu/cli/main.py
+++ logadu/cli/main.py
@@ -1,5 +1,5 @@
 import click
-from logadu.commands.parse import parse
+# from logadu.commands.parse import parse
 # from logadu.commands.merge import merge
 # from logadu.commands.seq import seq
 # from logadu.commands.train import train
@@ -7,17 +7,19 @@ from logadu.commands.parse import parse
 # from logadu.commands.predict import predict
 # from logadu.commands.evaluate import evaluate
 # from logadu.commands.convert import convert
-from logadu.commands.vectorize import vectorize
-from logadu.commands.vectorize_bert import vectorizebert
+# from logadu.commands.vectorize import vectorize
+# from logadu.commands.vectorize_bert import vectorizebert
 from logadu.commands.run import run
+
 
 @click.group()
 def cli():
     """LogADU - Advanced Log Analysis and Processing"""
     pass
 
+
 # Log parsing
-cli.add_command(parse)
+# cli.add_command(parse)
 # cli.add_command(merge)
 # # Feature Extraction
 # ## Sequence vectors
@@ -27,8 +29,8 @@ cli.add_command(parse)
 # ## Semantic vectors
 # # cli.add_command(semantic)
 # cli.add_command(represent)
-cli.add_command(vectorize)
-cli.add_command(vectorizebert)
+# cli.add_command(vectorize)
+# cli.add_command(vectorizebert)
 # cli.add_command(convert)
 cli.add_command(run)
 # # Training
